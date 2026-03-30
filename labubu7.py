f25a679cb5f5ae519bcedcb149ac3095bf3b0d17
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-03-30 19:47:59 -0300

    Update labubu7.py

--- labubu7.py
+++ labubu7.py
@@ -1,2 +1,14 @@
 import os
 os.system('cls')
+
+# Calculadora de descuento de compra:
+# Solicita al usuario que ingrese el precio original del artículo y el porcentaje de descuento.
+# Calcula el precio final después del descuento.
+# Muestra el precio final al usuario.
+
+precio = float(input("Ingrese el precio del artículo: "))
+descuento = int(input("Ponga el descuento con números enteros: "))
+
+fprecio = precio * (descuento/100)
+
+print(f"El precio final, es de {fprecio}.")
