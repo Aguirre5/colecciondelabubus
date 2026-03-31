304a390cde109cf2ec2caa551341115aeb2440e7
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-03-31 08:08:29 -0300

    Update labubu8.py

--- labubu8.py
+++ labubu8.py
@@ -1,6 +1,27 @@
 import os
 os.system('cls')
 
+# Un generador de contraseñas aleatorias crea combinaciones seguras de caracteres alfanuméricos y símbolos para proteger cuentas en línea. Su uso es fundamental para evitar ataques de fuerza bruta y mantener la seguridad digital.
+#
+#
+#
+
 import random
 
-password = input("Ingresa cuantos carácteres quieres que tenga tú contraseña aesthethic: ")
+password = int(input("Ingresa cuantos carácteres quieres que tenga tú contraseña aesthethic: "))
+
+minus = "abcdefghijklmnopqrstuvwxyz"
+mayu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nmrs = "0123456789"
+simb = "!@#$%^&*()-_=+[]{};:,.<>?/|"
+
+posibilidades = minus + mayu + nmrs + simb
+
+randompassword = ""
+
+for i in range(password):
+    i = random.randint(0, len(posibilidades)- 1)
+    randompassword = randompassword + posibilidades[i]
+    
+print("Contraseña lista: ")
+print(f"{randompassword}")
